a2d83347ba3fe25a79d14e2114abd002ab440d9b
dtrabuc2 <dtrabuc32@gmail.com>
2026-09-20 16:29:48 +0200

    Nettoyer le layout MVC et les partials

--- App/View/layout/footer.php
+++ App/View/layout/footer.php
@@ -5,21 +5,36 @@
                 <h2 class="h5 fw-bold">Vite &amp; Gourmand</h2>
                 <p class="small mb-0">Traiteur événementiel à Bordeaux : des menus préparés pour vos repas privés et professionnels.</p>
             </div>
+
             <div class="col-md-5">
                 <h2 class="h6 fw-bold">Horaires</h2>
                 <ul class="list-unstyled small mb-0">
                     <?php
-                    $days = [1 => 'Lundi', 2 => 'Mardi', 3 => 'Mercredi', 4 => 'Jeudi', 5 => 'Vendredi', 6 => 'Samedi', 7 => 'Dimanche'];
+                    $days = [
+                        1 => 'Lundi',
+                        2 => 'Mardi',
+                        3 => 'Mercredi',
+                        4 => 'Jeudi',
+                        5 => 'Vendredi',
+                        6 => 'Samedi',
+                        7 => 'Dimanche',
+                    ];
                     foreach ($openingHours ?? [] as $hour):
                     ?>
-                        <li><?= $escape($days[(int) $hour['day_of_week']] ?? '') ?> :
+                        <li>
+                            <?= $escape($days[(int) $hour['day_of_week']] ?? '') ?> :
                             <?php if ((int) $hour['is_open'] === 1): ?>
-                                <?= $escape(substr((string) $hour['opening_time'], 0, 5)) ?> - <?= $escape(substr((string) $hour['closing_time'], 0, 5)) ?>
-                            <?php else: ?>Fermé<?php endif; ?>
+                                <?= $escape(substr((string) $hour['opening_time'], 0, 5)) ?>
+                                -
+                                <?= $escape(substr((string) $hour['closing_time'], 0, 5)) ?>
+                            <?php else: ?>
+                                Fermé
+                            <?php endif; ?>
                         </li>
                     <?php endforeach; ?>
                 </ul>
             </div>
+
             <div class="col-md-3">
                 <h2 class="h6 fw-bold">Informations</h2>
                 <a class="link-light small d-block" href="/menus">Nos menus</a>
@@ -31,11 +46,10 @@
         </div>
     </div>
 </footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
 <script src="/assets/js/api.js" defer></script>
 <script src="/assets/js/OrderPriceCalculator.js" defer></script>
 <script src="/assets/js/script.js" defer></script>
 <script src="/assets/js/menus.js" defer></script>
 <script src="/assets/js/order.js" defer></script>
-</body>
-</html>
