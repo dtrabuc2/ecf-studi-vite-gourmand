d27c4723cfed3c9b470eb14c434962b72946ee36
dtrabuc2 <dtrabuc32@gmail.com>
2026-09-20 14:55:14 +0200

    Mettre à jour les scripts du frontend MVC

--- App/View/layout/footer.php
+++ App/View/layout/footer.php
@@ -12,9 +12,9 @@
                     $days = [1 => 'Lundi', 2 => 'Mardi', 3 => 'Mercredi', 4 => 'Jeudi', 5 => 'Vendredi', 6 => 'Samedi', 7 => 'Dimanche'];
                     foreach ($openingHours ?? [] as $hour):
                     ?>
-                        <li><?= $escape($days[(int)$hour['day_of_week']] ?? '') ?> :
-                            <?php if ((int)$hour['is_open'] === 1): ?>
-                                <?= $escape(substr((string)$hour['opening_time'], 0, 5)) ?> - <?= $escape(substr((string)$hour['closing_time'], 0, 5)) ?>
+                        <li><?= $escape($days[(int) $hour['day_of_week']] ?? '') ?> :
+                            <?php if ((int) $hour['is_open'] === 1): ?>
+                                <?= $escape(substr((string) $hour['opening_time'], 0, 5)) ?> - <?= $escape(substr((string) $hour['closing_time'], 0, 5)) ?>
                             <?php else: ?>Fermé<?php endif; ?>
                         </li>
                     <?php endforeach; ?>
@@ -31,9 +31,11 @@
         </div>
     </div>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
 <script src="/assets/js/api.js" defer></script>
 <script src="/assets/js/OrderPriceCalculator.js" defer></script>
 <script src="/assets/js/script.js" defer></script>
 <script src="/assets/js/menus.js" defer></script>
+<script src="/assets/js/order.js" defer></script>
 </body>
 </html>
