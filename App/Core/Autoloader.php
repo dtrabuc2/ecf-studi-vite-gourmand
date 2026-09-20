d03db507853203b6761693a661da9e957963e614
dtrabuc2 <dtrabuc32@gmail.com>
2026-09-20 16:26:28 +0200

    Retirer les anciens bootstrap de configuration

--- App/Core/Autoloader.php
+++ App/Core/Autoloader.php
@@ -1,11 +1,8 @@
 <?php
+declare(strict_types=1);
 
-spl_autoload_register(function ($class) {
-
-  if (file_exists('../' . str_replace('\\', '/', $class) . '.php')) {
-    require_once '../' . str_replace('\\', '/', $class) . '.php';
-  }
-});
-
-require_once 'config.php';
-require_once 'functions.php';
+/*
+ * Le projet utilise Composer et PSR-4 pour l'autoloading.
+ * Ce fichier est conservé uniquement pour ne pas casser les références historiques.
+ */
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
