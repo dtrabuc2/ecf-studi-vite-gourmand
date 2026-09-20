78f7e02d8e376b3b19cd4deba9e281170e2d6b85
dtrabuc2 <dtrabuc32@gmail.com>
2026-09-20 20:06:01 +0200

    fix: corriger les incohérences du refactor backend

--- App/Core/Exception/AuthenticationException.php
+++ App/Core/Exception/AuthenticationException.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Core\Exception;
 
@@ -7,18 +8,11 @@ use Throwable;
 
 class AuthenticationException extends Exception
 {
-
-  public function insert($message, $code = 0, Throwable $previous = null)
-  {
-    parent::__construct($message, $code, $previous);
-  }
-
-  // custom string representation of object
-  public function __toString()
-  {
-    return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
-  }
-
-
-
+    public function __construct(
+        string $message = '',
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
 }
