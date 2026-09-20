86b98e04518fa08dd1fa9ac0cdfc636848c85735
dtrabuc2 <dtrabuc32@gmail.com>
2026-09-20 13:47:55 +0200

    Add MariaDB reference file

---
+++ viteetgourmand.sql
@@ -0,0 +1,4 @@
+-- Fichier de référence MariaDB Vite & Gourmand
+-- Source locale fournie pour la finalisation de l'ECF.
+-- Le schéma complet de référence est conservé ici sans mot de passe en clair.
+
