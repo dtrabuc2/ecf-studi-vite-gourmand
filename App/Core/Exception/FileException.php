28c4c6576cf979c1875842cbd75cb654f92113f8
dtrabuc2 <dtrabuc32@gmail.com>
2026-09-20 20:06:21 +0200

    fix: corriger les exceptions legacy du backend

--- App/Core/Exception/FileException.php
+++ App/Core/Exception/FileException.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Core\Exception;
 
@@ -7,17 +8,11 @@ use Throwable;
 
 class FileException extends Exception
 {
-
-  public function insert($message, $code = 0, Throwable $previous = null)
-  {
-    parent::__construct($message, $code, $previous);
-  }
-
-  public function __toString()
-  {
-    return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
-  }
-
-
-
+    public function __construct(
+        string $message = '',
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
 }
