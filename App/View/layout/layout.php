38f69639a5d9fb33f35b4ac81eaa074079bd7ebd
dtrabuc2 <dtrabuc32@gmail.com>
2026-09-21 06:11:17 +0200

    utiliser le nouveau découpage CSS

--- App/View/layout/layout.php
+++ App/View/layout/layout.php
@@ -6,7 +6,22 @@
     <meta name="csrf-token" content="<?= $escape($csrfToken) ?>">
     <title><?= $escape($title) ?></title>
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/tokens.css">
+    <link rel="stylesheet" href="/assets/css/base.css">
+    <link rel="stylesheet" href="/assets/css/components/navbar.css">
+    <link rel="stylesheet" href="/assets/css/components/buttons.css">
+    <link rel="stylesheet" href="/assets/css/components/cards.css">
+    <link rel="stylesheet" href="/assets/css/components/forms.css">
+    <link rel="stylesheet" href="/assets/css/components/footer.css">
+    <link rel="stylesheet" href="/assets/css/components/hero.css">
+    <link rel="stylesheet" href="/assets/css/components/feedback.css">
+    <link rel="stylesheet" href="/assets/css/pages/home.css">
+    <link rel="stylesheet" href="/assets/css/pages/menus.css">
+    <link rel="stylesheet" href="/assets/css/pages/orders.css">
+    <link rel="stylesheet" href="/assets/css/pages/admin.css">
+    <link rel="stylesheet" href="/assets/css/pages/auth.css">
+    <link rel="stylesheet" href="/assets/css/pages/contact.css">
+    <link rel="stylesheet" href="/assets/css/pages/quote.css">
     <link rel="stylesheet" href="/assets/css/responsive.css">
 </head>
 <body class="d-flex flex-column min-vh-100 bg-light"
