d32e7bd32180f2f275780cb1b721a1f3a13f03df
dtrabuc2 <dtrabuc32@gmail.com>
2026-09-21 08:48:50 +0200

    Finaliser le nettoyage des images et du parcours à la carte

--- App/View/home/menus.php
+++ App/View/home/menus.php
@@ -57,9 +57,7 @@
         <div id="menuGrid" class="row g-4 mb-5">
             <?php foreach ($menus as $menu): ?>
                 <?php $details = $menuDetails[$menu->getId()] ?? ['dishes' => []]; ?>
-                <?php $mongoImages = $menuImages[$menu->getId()] ?? []; ?>
-                <?php $cover = $mongoImages[0] ?? null; ?>
-                <div class="col-md-6 col-lg-4" data-menu-card>
+<div class="col-md-6 col-lg-4" data-menu-card>
                     <article class="card h-100 border-0 shadow-sm overflow-hidden">
                         <?php if ($cover && !empty($cover['url'])): ?>
                             <img src="<?= $escape($cover['url']) ?>" class="card-img-top home-menu-image" alt="<?= $escape($cover['alt_text'] ?? $menu->getTitle()) ?>">
